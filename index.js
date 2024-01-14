a34524aeffc5f638c3af3684365871f0fd67a9b8
jay7828 <122548702+jay7828@users.noreply.github.com>
2024-01-15 00:35:38 +0530

    14-01 user schema And Register Api

---
+++ index.js
@@ -0,0 +1,18 @@
+const express =require('express')
+const app = express()
+
+const mongoDB=require("mongoose")
+const mongoURI='mongodb+srv://jayesh:jayeshsharma@cluster0.8bukqbm.mongodb.net/notedb'
+mongoDB.connect(mongoURI).then(function()
+{
+    app.get('/',(req,res)=>{
+        res.send('Hello World')
+    })
+    app.use(express.json())
+    app.use('/api/',require("./Routes/register"))
+   
+})
+const PORT = process.env.PORT||5000
+app.listen(PORT ,()=>{
+    console.log(`Server is running on ${PORT}`)
+})
