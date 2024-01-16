027eb7ca638c7477a1b32614517968283164ead7
jay7828 <js758089@gmail.com>
2024-01-16 21:24:23 +0530

    2

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const express =require('express')
 const app = express()
 const mongoDB=require("mongoose")
 const mongoURI='mongodb+srv://jayesh:jayeshsharma@cluster0.8bukqbm.mongodb.net/notedb'
+
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin")
+    res.header(
+        "Allow-Control-Allow-Header",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    next();
+})
+
 mongoDB.connect(mongoURI).then(function()
 {
     app.get('/',(req,res)=>{
-        res.send('Hello World')
+        res.send('API Works')
     })
     app.use(express.json())
     app.use('/api/',require("./Routes/register"))
-   
 })
 const PORT = process.env.PORT||5000
 app.listen(PORT ,()=>{
