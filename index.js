12b3a82d12aac41de6ca60c5b70f74150a01ca7c
Abhishek Choudhary <abhikhati2212@gmail.com>
2024-02-03 15:01:05 +0530

    update profile on route api/user/profile/userId also add env file

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const express =require('express')
-const app = express()
-const mongoDB=require("mongoose")
-const mongoURI='mongodb+srv://jayesh:toconnect@userdetails.fdxtamm.mongodb.net/?retryWrites=true&w=majority'
+const express = require("express");
+const app = express();
+const mongoDB = require("mongoose");
+require("dotenv").config();
+const mongoURI = process.env.MONGODB_URL;
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-    next();
-  });
-  
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
 
-mongoDB.connect(mongoURI).then(function()
-{
-    app.get('/',(req,res)=>{
-        res.send('API Works')
-    })
-    app.use(express.json())
-    app.use('/api/',require("./Routes/register"))
-    app.use('/api/project/',require("./Routes/addProject"))
-})
-const PORT = process.env.PORT||5000
-app.listen(PORT ,()=>{
-    console.log(`Server is running on ${PORT}`)
-})
+mongoDB.connect(mongoURI).then(function () {
+  app.get("/", (req, res) => {
+    res.send("API Works");
+  });
+  app.use(express.json());
+  app.use("/api/", require("./Routes/register"));
+  app.use("/api/project/", require("./Routes/addProject"));
+  app.use("/api/users/", require("./Routes/userRoutes"));
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
