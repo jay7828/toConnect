3dabc79cacb03c26fa7cf9954345ff9f2b436fac
SanskarGour <sanskargour1234@gmail.com>
2024-02-19 13:41:39 +0530

    pid

--- model/collabletter_Schema.js
+++ model/collabletter_Schema.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const collab_letterSchema = new mongoose.Schema({
+    
+    pid:{type:String , required:true},
+    ptitle:{type:String , required:true},
     letterID:{type:String , required:true},
     sender: { type: String,required:true },
     receiver: { type: String, required: true },
