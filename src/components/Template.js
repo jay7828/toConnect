761ed7931d302cbc6d0e3f2b0cf0988a900dc4fd
jay7828 <js758089@gmail.com>
2024-02-20 12:52:54 +0530

    m

--- src/components/Template.js
+++ src/components/Template.js
@@ -3,8 +3,21 @@ import SignupForm from "./SignupForm";
 import LoginForm from "./LoginForm";
 import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
-
+import axios from "axios";
 function Template({ formtype }) {
+  async function googleLogin(response) {
+    // const decodedHeader = jwtDecode(responseGoogle.credential);
+    // console.log(decodedHeader);
+    console.log(response);
+    await axios({
+      method: "POST",
+      url: "http://localhost:google/api/googlelogin",
+      data: { tokenId: response.tokenId },
+    }).then((response) => {
+      console.log(response);
+    });
+  }
+
   return (
     <div className="flex justify-center items-center w-[100vw] h-[100vh]">
       <div className="h-max max-w-[30vw] min-w-max flex-col gap-4 justify-start items-center">
@@ -18,9 +31,8 @@ function Template({ formtype }) {
 
         <div className="flex justify-center">
           <GoogleLogin
-            onSuccess={(credentialResponse) => {
-              const decodedHeader = jwtDecode(credentialResponse.credential);
-              console.log(decodedHeader);
+            onSuccess={(responseGoogle) => {
+              googleLogin(responseGoogle);
             }}
             onError={() => {
               console.log("Login Failed");
